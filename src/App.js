1ec8be0ee6eda2333139abbca8d67ccd6187b017
Pablo Embarach <pabloembarach@gmail.com>
2023-09-19 19:01:39 +0000

    Create and add a Banner and Menu navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Banner from './Banner';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Banner/>
     </div>
   );
 }
---
+++ src/Banner/index.js
@@ -0,0 +1,22 @@
+import './banner.css'
+import * as React from 'react';
+import  Navbar from '../Navbar';
+
+const Banner = () => {
+
+  return (
+    <header className='banner'>
+      <img src='/image/img.png' alt='Pablo`s' />
+      <div className='title'>
+        <h1>PABLO EMBARACH</h1>
+        <h2>Frontend Developer</h2>
+        <h3>&lt;turning ideas into reality through codes&gt;</h3>
+        <Navbar/>
+      </div>
+    </header>
+  )
+}
+
+export default Banner
+
+
